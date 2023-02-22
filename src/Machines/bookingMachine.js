b053ae3d5e2e743e08f3490ada25833f16bd84b9
Simón A Azócar <saimn.azocar@gmail.com>
2023-02-21 20:25:37 -0400

    ADD actions

--- src/Machines/bookingMachine.js
+++ src/Machines/bookingMachine.js
@@ -1,34 +1,50 @@
 import { createMachine } from "xstate";
 
-const bookingMachine = createMachine({
-  id: 'Buy plane tickets',
-  initial: 'initial',
-  states: {
-    initial: {
-      on: {
-        START: 'search',
+const bookingMachine = createMachine(
+  {
+    id: 'Buy plane tickets',
+    initial: 'initial',
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: 'search',
+            actions: 'printStart',
+          },
+        },
       },
-    },
-    search: {
-      on: {
-        CONTINUE: 'passengers',
-        CANCEL: 'initial',
+      search: {
+        entry: 'printEntry',
+        exit: 'printExit',
+        on: {
+          CONTINUE: 'passengers',
+          CANCEL: 'initial',
+        },
       },
-    },
-    passengers: {
-      on: {
-        DONE: 'tickets',
-        CANCEL: 'initial',
+      passengers: {
+        on: {
+          DONE: 'tickets',
+          CANCEL: 'initial',
+        },
       },
-    },
-    tickets: {
-      on: {
-        FINISH: 'initial',
-        CANCEL: 'initial',
+      tickets: {
+        on: {
+          FINISH: 'initial',
+          CANCEL: 'initial',
+        },
       },
     },
   },
-})
+  {
+    actions: {
+      printStart: () => console.log('Printing start'),
+      printEntry: () => console.log('Printing entry'),
+      printExit: () => console.log('Printing exit'),
+    },
+  }
+)
+
+
 
 
 export default bookingMachine;
