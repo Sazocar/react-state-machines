7cfa9bf98a44f20d2611e139618a1482b51105e9
Simón A Azócar <saimn.azocar@gmail.com>
2023-02-23 21:03:25 -0400

    ADD spinner

---
+++ src/Components/Spinner.js
@@ -0,0 +1,16 @@
+const Spinner = () => {
+  return (
+    <div class='dot-spinner'>
+      <div class='dot-spinner__dot'></div>
+      <div class='dot-spinner__dot'></div>
+      <div class='dot-spinner__dot'></div>
+      <div class='dot-spinner__dot'></div>
+      <div class='dot-spinner__dot'></div>
+      <div class='dot-spinner__dot'></div>
+      <div class='dot-spinner__dot'></div>
+      <div class='dot-spinner__dot'></div>
+    </div>
+  )
+}
+
+export { Spinner }
